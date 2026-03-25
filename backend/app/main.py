ff74ea7445b7b482d054dacf81e3a26d975dfb16
Thamini Sri B <minithaa2000@gmail.com>
2026-03-25 12:49:02 +0530

    BaCKeND

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.upload import router as upload_router
 from app.database.db import init_db, seed_data
 
 init_db()
 seed_data()
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(upload_router)
 
 @app.get("/")
